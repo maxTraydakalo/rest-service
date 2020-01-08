d62f5e0e87a552f458af5fe3c7ba9e84e8e5dc36
maxTraydakalo <max.traydakalo@gmail.com>
2020-01-08 11:09:34 +0200

    add ClaimRepository

---
+++ src/main/java/epam/rd/traydakalo/repository/ClaimRepository.java
@@ -0,0 +1,7 @@
+package epam.rd.traydakalo.repository;
+
+import epam.rd.traydakalo.entity.Claim;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClaimRepository extends CrudRepository<Claim, Long> {
+}
