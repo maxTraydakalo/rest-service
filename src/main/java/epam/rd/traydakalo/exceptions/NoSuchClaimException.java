6432f4de76c4573c5f0c146096ce9193e03349f3
maxTraydakalo <max.traydakalo@gmail.com>
2020-01-08 11:11:04 +0200

    add NoSuchClaimException, used in ClaimService

---
+++ src/main/java/epam/rd/traydakalo/exceptions/NoSuchClaimException.java
@@ -0,0 +1,8 @@
+package epam.rd.traydakalo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class NoSuchClaimException extends RuntimeException{
+}
