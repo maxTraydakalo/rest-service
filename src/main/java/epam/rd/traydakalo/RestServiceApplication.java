ebf370c89341158cff8e6d2e010f03c99ca2ad95
maxTraydakalo <max.traydakalo@gmail.com>
2020-01-08 11:28:31 +0200

    add Swagger

--- src/main/java/epam/rd/traydakalo/RestServiceApplication.java
+++ src/main/java/epam/rd/traydakalo/RestServiceApplication.java
@@ -1,4 +1,4 @@
-package epam.rd.traydakalo.restservice;
+package epam.rd.traydakalo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
