9687b3593edc0846e5c6c62959aca75110253ccd
maxTraydakalo <max.traydakalo@gmail.com>
2020-01-08 11:10:04 +0200

    add ClaimService

---
+++ src/main/java/epam/rd/traydakalo/service/ClaimService.java
@@ -0,0 +1,34 @@
+package epam.rd.traydakalo.service;
+
+import epam.rd.traydakalo.entity.Claim;
+import epam.rd.traydakalo.exceptions.NoSuchClaimException;
+import epam.rd.traydakalo.repository.ClaimRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClaimService {
+    ClaimRepository claimRepository;
+
+    public ClaimService(ClaimRepository claimRepository) {
+        this.claimRepository = claimRepository;
+    }
+
+    public Claim save(Claim claim) {
+        return claimRepository.save(claim);
+    }
+
+    public Claim findById(Long id) {
+        return claimRepository.findById(id).orElseThrow(NoSuchClaimException::new);
+    }
+
+    public Claim updateClaim(Claim claim) {
+        if (!claimRepository.existsById(claim.getId())) {
+            throw new NoSuchClaimException();
+        }
+        return claimRepository.save(claim);
+    }
+
+    public void deleteClaim(Claim claim){
+        claimRepository.delete(claim);
+    }
+}
